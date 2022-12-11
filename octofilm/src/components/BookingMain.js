f38a494478c6c832f46cd2317f25a7b3f1cc3225
PopovaO1000 <popova.o.s1000@gmail.com>
2022-12-11 14:55:50 +0300

    Booked seats

--- octofilm/src/components/BookingMain.js
+++ octofilm/src/components/BookingMain.js
@@ -13,16 +13,47 @@ function BookingMain() {
    const time = searchParams.get('time');
    useEffect(()=>{
       getOneSeans(film,date,time).then((seansObj)=>{
-         // setBooking(seansrow.map((seans)=>{
-         //     })
-         // );
-         console.log(seansObj)
+         const seats = seansObj.zal.split(' ');
+         const bookedSeats = seansObj.notFreePlaces;
+          setBooking(seats.map((maxSeat,i)=>{
+             let seat = 1;
+             let seatArr = [];
+             let isRowBooked = false;
+             let bookedSeatsArr = [];
+             bookedSeats.forEach((bookedSeatRow,j)=>{
+                 if(i+1 == bookedSeatRow.num){
+                     isRowBooked = true;
+                     bookedSeatRow.innerArr.forEach((bookedSeatt,m)=>{
+                         bookedSeatsArr.push(bookedSeatt);
+                     })
+                 }
+             });
+             while(seat<=maxSeat){
+                 let className = "seat";
+                 if(isRowBooked){
+                     bookedSeatsArr.forEach((bookedSeatt,j)=>{
+                         if(seat == bookedSeatt){
+                             className += " is_booked";
+                         }
+                     });
+                 }
+                 if(seat == 1){
+                     className += " seat_gap";
+                 }
+                 seatArr.push(<button key={i+seat} id={i+1+'-'+seat} className={className}></button>);
+                 seat += 1;
+             }
+             return seatArr;
+         })
+          );
       });
    },[]);
    return (
        <main className="NewsMain">
           <section>
-             vkus
+              <article className="Booking_seats">
+                  {booking}
+              </article>
           </section>
        </main>
    );
